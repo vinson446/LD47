32d03e16948182e65e711380cdbbeb52c2d2a2fb
Vinson Kok <vinsonkok@yahoo.com>
2020-10-02 19:35:40 -0500

    Player/Camera Control

---
+++ Assets/Scripts/Player/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float mouseSensitivity = 100;
+    public Transform playerBody;
+
+    float xRot = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // rotate camera when moving mouse vertically
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+        transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+
+        // rotate player when moving mouse horizontally
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed;
+    public float sprintSpeed;
+    public bool isMoving;
+    public bool isWalking;
+
+    public float jumpSpeed;
+    public float gravity;
+
+    Vector3 moveDirection = Vector3.zero;
+    CharacterController charController;
+
+    [Header("Sound Settings")]
+    public float moveVolume;
+    public float movePitch;
+    public float moveInterval;
+    public float sprintingVolume;
+    public float sprintingPitch;
+    public float sprintingInterval;
+    public bool canPlaySound = true;
+
+    public AudioClip[] audioClips;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (charController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            if (moveDirection.x != 0 || moveDirection.z != 0)
+            {
+                isMoving = true;
+            }
+            else
+            {
+                isMoving = false;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isWalking = false;
+                moveDirection = moveDirection * sprintSpeed;
+            }
+            else
+            {
+                isWalking = true;
+                moveDirection = moveDirection * moveSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // move controller;
+        charController.Move(moveDirection * Time.deltaTime);
+
+        // play moving sound while player is moving
+        if (charController.isGrounded && isMoving && canPlaySound)
+        {
+            if (isWalking)
+            {
+                PlaySound(0, moveVolume, movePitch);
+            }
+            else
+            {
+                PlaySound(0, sprintingVolume, sprintingPitch);
+            }
+        }
+    }
+
+    void PlaySound(int index, float volume, float pitch)
+    {
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+
+        audioSource.PlayOneShot(audioClips[index]);
+        canPlaySound = false;
+
+        StartCoroutine(PlaySound());
+    }
+
+    IEnumerator PlaySound()
+    { 
+        if (isWalking)
+        {
+            yield return new WaitForSeconds(moveInterval);
+            canPlaySound = true;
+        }
+        else
+        {
+            yield return new WaitForSeconds(sprintingInterval);
+            canPlaySound = true;
+        }
+    }
+}
